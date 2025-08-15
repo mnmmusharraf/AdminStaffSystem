2f60b82769ad497cac7bb1752d91db80b08ed35c
mnm musharraf <mnmmusharraf@gmail.com>
2025-08-15 19:50:21 +0530

    DAO Test Null Fixed in StaffDAO

--- src/test/java/com/pahana/adminstaffsystem/dao/StaffDAOTest.java
+++ src/test/java/com/pahana/adminstaffsystem/dao/StaffDAOTest.java
@@ -25,7 +25,8 @@ public class StaffDAOTest {
 
     @BeforeEach
     public void setUp() {
-        createdStaffId = null;
+        dao = new StaffDAO();
+        //createdStaffId = null;
     }
 
     @AfterEach
